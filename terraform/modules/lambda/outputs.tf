40ac96fad4b397347890e7abf49240b5b740e173
Asadul <mddipu@gmail.com>
2024-10-17 11:51:53 +0200

    Modifying the github actions workflow.

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -3,13 +3,11 @@ output "lambda_function_arn" {
     value = aws_lambda_function.lambda_function.arn
 }
 
-# Output the Lambda function name
 output "lambda_function_name" {
   description = "The name of the Lambda function"
   value       = aws_lambda_function.lambda_function.function_name
 }
 
-# Output the Lambda function invoke ARN
 output "lambda_invoke_arn" {
   description = "The invoke ARN of the Lambda function"
   value       = aws_lambda_function.lambda_function.invoke_arn
